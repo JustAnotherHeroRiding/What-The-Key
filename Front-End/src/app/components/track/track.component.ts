498ef3e55b1ff4a9e6e2756c0020bfab9ceee4e9
Kristijan Kocev <kristijankocev1234@gmail.com>
2023-12-26 18:55:28 +0100

    reordering of the restore icon on small screen sizes

--- Front-End/src/app/components/track/track.component.ts
+++ Front-End/src/app/components/track/track.component.ts
@@ -3,6 +3,12 @@ import { TrackData } from '../../pages/home/home.component';
 import { getNoteName } from '../result-card/result-card.component';
 import { Router } from '@angular/router';
 
+export enum TrackAction {
+  Delete,
+  Restore,
+  AddTabs,
+}
+
 @Component({
   selector: 'app-track',
   templateUrl: './track.component.html',
@@ -12,22 +18,30 @@ export class TrackComponent {
   @Input() trackData!: TrackData;
   @Output() deleteRequest = new EventEmitter<TrackData>();
   @Output() restore = new EventEmitter<TrackData>();
-
+  @Output() addTabs = new EventEmitter<TrackData>();
+  trackActions = TrackAction;
   constructor(private router: Router) {}
 
   getNoteDisplayName(noteValue: number): string {
     return getNoteName(noteValue);
   }
 
-  requestDelete(event: Event) {
+  handleAction(event: Event, action: TrackAction) {
     event.stopPropagation();
 
-    this.deleteRequest.emit(this.trackData);
-  }
-
-  requestRestore(event: Event) {
-    event.stopPropagation();
-    this.restore.emit(this.trackData);
+    switch (action) {
+      case TrackAction.Delete:
+        this.deleteRequest.emit(this.trackData);
+        break;
+      case TrackAction.Restore:
+        this.restore.emit(this.trackData);
+        break;
+      case TrackAction.AddTabs:
+        this.addTabs.emit(this.trackData);
+        break;
+      default:
+        alert('Action is not supported.');
+    }
   }
 
   goToTrackPage(trackId: string) {
